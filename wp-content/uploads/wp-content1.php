ee0cf9b9e57262bde494210b269de2547b5239f1
odtugastronomi <odtugastronomi@example.com>
2018-08-26 18:32:39 +0000

    Initial commit

---
+++ wp-content/uploads/wp-content1.php
@@ -0,0 +1,8 @@
+<br />
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function set_magic_quotes_runtime() in /var/www/html/wp-content/uploads/wp-content1.php(1) : eval()'d code(3) : eval()'d code(1) : eval()'d code:20
+Stack trace:
+#0 /var/www/html/wp-content/uploads/wp-content1.php(1) : eval()'d code(3) : eval()'d code(1): eval()
+#1 /var/www/html/wp-content/uploads/wp-content1.php(1) : eval()'d code(3): eval()
+#2 /var/www/html/wp-content/uploads/wp-content1.php(1): eval()
+#3 {main}
+  thrown in <b>/var/www/html/wp-content/uploads/wp-content1.php(1) : eval()'d code(3) : eval()'d code(1) : eval()'d code</b> on line <b>20</b><br />
